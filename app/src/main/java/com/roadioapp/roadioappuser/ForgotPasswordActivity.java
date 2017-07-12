3e61569b84c5f02484aa696cf89530763a3f8c76
Muhammad Saqib <mohammadsaqib1997@gmail.com>
2017-07-12 12:27:38 +0500

    Forgot Password Working

--- app/src/main/java/com/roadioapp/roadioappuser/ForgotPasswordActivity.java
+++ app/src/main/java/com/roadioapp/roadioappuser/ForgotPasswordActivity.java
@@ -1,16 +1,59 @@
 package com.roadioapp.roadioappuser;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.roadioapp.roadioappuser.mObjects.ButtonEffects;
 
 public class ForgotPasswordActivity extends AppCompatActivity {
+
+    private Activity activity;
+
+    private ButtonEffects buttonEffects;
+
+    private LinearLayout subBtn;
+    private EditText mobNumber;
+
+    private String mob_no_str;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+        activity = this;
+
+        buttonEffects = new ButtonEffects(activity);
+
+        mobNumber = (EditText) findViewById(R.id.mobNumber);
+
+        subBtn = (LinearLayout) findViewById(R.id.subBtn);
+        buttonEffects.btnEventEffRounded(subBtn);
+        subBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mob_no_str = mobNumber.getText().toString().trim();
+                String err = "";
+                if(mob_no_str.isEmpty()){
+                    err = "Mobile Number is required!";
+                }else if(mob_no_str.length() != 12){
+                    err = "Mobile Number is invalid!";
+                }
+
+                if(!err.isEmpty()){
+                    Toast.makeText(activity, err, Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(activity, "valid", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     @Override
